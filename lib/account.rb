78bf997bb032c33d861214a760d2b20c72791911
yurizhuravel <zhuravel@gmail.com>
2016-09-19 11:19:51 +0100

    Refactoring

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,10 @@
 class Account
+
   STARTING_BALANCE = 0
   attr_reader :balance
 
-  def initialize(balance = STARTING_BALANCE)
-    @balance = balance
+  def initialize
+    @balance = STARTING_BALANCE
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,9 +2,9 @@ require 'account'
 
 describe Account do
 
-  subject(:account) { described_class.new(balance) }
+  subject(:account) { described_class.new }
 
-  it 'new account balance is 0' do
+  it 'new account balance is 0 by default' do
     expect(account.balance).to eq 0
   end
 
